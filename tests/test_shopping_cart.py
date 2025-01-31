f8e22e72903a6b3fd74364b5bfc2ed8ce3954123
Janira Huesca <janirahuesca@gmail.com>
2025-01-31 17:46:59 +0100

    fix(all tests): Implemented the corrections suggested by Reyes Cuesta
    - Removed strict URL assertions to make tests more resilient.
    - Removed the valid search scenario, because this scenario was already tested by the shopping cart feature
    - Removed the add item to shopping cart because with the remove item from shopping cart we're already testing the search feature and the add to cart scenario
    - Fixed incorrect log message in 'signup with empty name' (changed "fill with invalid email" to "fill with valid email")
    - Removed the visit_page test
    - Some other small refactors done

--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -1,39 +1,37 @@
 from playwright.sync_api import Page, expect
+import utils
 
-# SCENARIO 1 (adding an item to the shopping cart)
-def test_add_item_to_cart(page:Page):
-    print("Given the user visits signup page")
+# SCENARIO 1 (searching an item, adding it to the shopping cart and removing it from the shopping cart)
+def test_search_add_remove_item_to_cart(page:Page):
+    print("Given the user visits 'La Casa del Libro' homepage")
     # Navigation to open the URL in the browser
-    page.goto("https://www.casadellibro.com/")
+    utils.visit_homepage(page)
 
     print("And the user accepts the cookies")
-    # Wait until the banner to accept the cookies appears
-    page.wait_for_selector("button:has-text('Aceptar')")
-    # Locate the element by role (button) to accept the cookies and click on it
-    page.get_by_role("button", name="Aceptar").click()
+    utils.accept_cookies(page)
 
-    print("And the user clicks on the search bar")
+    print("When the user performs a search")
     # Locate the element by placeholder and click on it
+    page.wait_for_timeout(1000)
     page.get_by_placeholder("Busca por autor, título, género, ISBN").click()
-
-    print("And the user clicks on the search input")
     # Locate the element by locator and click on it
+    page.wait_for_timeout(1000)
     page.locator("[data-test=\"search-input\"]").click()
     # Locate the element by locator and clear the content
     page.locator("[data-test=\"search-input\"]").clear()
-
-    print("And the user fills the search input with the search that want to perform")
-    # Fill the search input with a valid search
-    content_searched = "harry potter"
-    page.locator("[data-test=\"search-input\"]").fill(content_searched)
-
-    print("And the user press 'Enter' in the keyboard")
+    # Fill the search input with an valid search
+    valid_content_searched = "harry potter"
+    page.locator("[data-test=\"search-input\"]").fill(valid_content_searched)
     # Press intro
     page.locator("[data-test=\"search-input\"]").press("Enter")
 
-    print("And the user is redirected to the page with its search")
-    # Verify that the query contains the search
-    expect(page).to_have_url("https://www.casadellibro.com/?query=harry%20potter")
+    print("Then the user should see a message indicating how many results there are with its search")
+    # Verify that there are results for the search and that the message is visible
+    expect(page.get_by_text(f"resultados para {valid_content_searched}")).to_be_visible()
+
+    print("And the user should see results containing its search")
+    # Verify that the results shown are related to the search performed
+    expect(page.locator("[data-test=\"base-grid\"]")).to_contain_text(valid_content_searched)
 
     print("When the user clicks on the desired result")
     # Click on one of the results, in this case 'HARRY POTTER Y LA PIEDRA FILOSOFAL'
@@ -48,57 +46,6 @@ def test_add_item_to_cart(page:Page):
     expect(page.locator("div").filter(has_text="Tu cesta").nth(3)).to_be_visible()
 
     print("And the user should see that the item selected has been added to the shopping cart")
-    # Verify that the item that there is in the shopping cart is the item selected
-    expect(page.locator("#t-p-cl")).to_contain_text("HARRY POTTER Y LA PIEDRA FILOSOFAL")
-
-    # SCENARIO 2 (removing an item from the shopping cart)
-def test_remove_item_from_cart(page:Page):
-    print("Given the user visits signup page")
-    # Navigation to open the URL in the browser
-    page.goto("https://www.casadellibro.com/")
-
-    print("And the user accepts the cookies")
-    # Wait until the banner to accept the cookies appears
-    page.wait_for_selector("button:has-text('Aceptar')")
-    # Locate the element by role (button) to accept the cookies and click on it
-    page.get_by_role("button", name="Aceptar").click()
-
-    print("And the user clicks on the search bar")
-    # Locate the element by placeholder and click on it
-    page.get_by_placeholder("Busca por autor, título, género, ISBN").click()
-
-    print("And the user clicks on the search input")
-    # Locate the element by locator and click on it
-    page.locator("[data-test=\"search-input\"]").click()
-    # Locate the element by locator and clear the content
-    page.locator("[data-test=\"search-input\"]").clear()
-
-    print("And the user fills the search input with the search that want to perform")
-    # Fill the search input with a valid search
-    content_searched = "harry potter"
-    page.locator("[data-test=\"search-input\"]").fill(content_searched)
-
-    print("And the user press 'Enter' in the keyboard")
-    # Press intro
-    page.locator("[data-test=\"search-input\"]").press("Enter")
-
-    print("And the user is redirected to the page with its search")
-    # Verify that the query contains the search
-    expect(page).to_have_url("https://www.casadellibro.com/?query=harry%20potter")
-
-    print("And the user clicks on the desired result")
-    # Click on one of the results, in this case 'HARRY POTTER Y LA PIEDRA FILOSOFAL'
-    page.get_by_role("link", name="HARRY POTTER Y LA PIEDRA FILOSOFAL", exact=True).click()
-
-    print("And the user clicks on the button indicating the adding to cart action")
-    # Click on the 'Add to cart' button
-    page.get_by_role("button", name="Añadir a la cesta").click()
-
-    print("And the user sees the shopping cart section")
-    # Verify that the content of shopping cart is visible
-    expect(page.locator("div").filter(has_text="Tu cesta").nth(3)).to_be_visible()
-
-    print("And the user sees that the item selected has been added to the shopping cart")
     # Verify that the item that there is in the shopping cart is the item selected
     expect(page.locator("#t-p-cl")).to_contain_text("HARRY POTTER Y LA PIEDRA FILOSOFAL")
 
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,15 @@
 from playwright.sync_api import Page, expect
 import re
 
+def visit_homepage(page: Page):
+    page.goto("https://www.casadellibro.com")
+
+def accept_cookies(page: Page):
+    # Wait until the banner to accept the cookies appears
+    page.wait_for_selector("button:has-text('Aceptar')")
+    # Locate the element by role (button) to accept the cookies and click on it
+    page.get_by_role("button", name="Aceptar").click()
+
 def is_mobile(page:Page):
     desktop_size = 1024
     is_mobile = False
