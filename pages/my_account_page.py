dedfad93c0b2189156b28bb151c25fe93aab3aed
Janira Huesca <janirahuesca@gmail.com>
2025-02-10 19:31:57 +0100

    fix(my_account_page): Added a small delay before clicking on signup and login links, and changed the locator for the go_to_login function

--- pages/my_account_page.py
+++ pages/my_account_page.py
@@ -11,10 +11,12 @@ class MyAccountPage:
 
     def go_to_signup(self):
         """ Clicks on the 'Registrarme' link to navigate to the signup page """
+        self.page.wait_for_timeout(500) # Small delay to ensure UI stability
         self.page.get_by_role("link", name="Registrarme").click()
         expect(self.page).to_have_url(re.compile(".*register-access.*"))
 
     def go_to_login(self):
         """ Clicks on the 'Iniciar Sesión' link to navigate to the login page """
-        self.page.get_by_role("link", name="Iniciar sesión").click()
+        self.page.wait_for_timeout(500) # Small delay to ensure UI stability
+        self.page.locator("a#b-i-s-c").click()
         expect(self.page).to_have_url(re.compile(".*login-access.*"))
